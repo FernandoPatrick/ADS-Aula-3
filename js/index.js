17d6f16952cfe9d7ac645f3c7e428734af4c1911
FernandoPatrick <78447989+FernandoPatrick@users.noreply.github.com>
2021-03-27 15:02:16 -0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,23 @@
+$(document).ready(
+    function(){
+        alert("Pagina Carregou");
+    }
+);
+
+$(`#bt_inicio`).click(
+    function click_inicio(){
+        alert("voce clicou no inicio!");
+    }
+);
+
+$(`#bt_inicio`).click(
+    function click_inicio(){
+        alert("voce clicou no sobre!");
+    }
+);
+
+$(`#bt_inicio`).click(
+    function click_inicio(){
+        alert("voce clicou no cadastro!");
+    }
+);
